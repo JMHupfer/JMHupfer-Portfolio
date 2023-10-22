d18b8a93bc5056f52157ba5fe0ae6cb3f42019c6
JMHupfer <jmhupfer@gmail.com>
2023-10-22 03:00:07 -0600

    adjusting styling on homepage and adding logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,31 @@
 import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/Jacob-H.png'
-import './index.scss';
+// import
+import './index.scss'
+import Logo from './Logo'
+// import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+  // const [letterClass, setLetterClass] = useState('text-animate')
+  // const nameArray = ['J', 'a', 'c', 'o', 'b', 'H', 'u', 'p', 'f', 'e', 'r']
 
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi, I'm Jacob Hupfer</h1>
-                <h2>Frontend Developer with heavy interest in learning more backend concepts</h2>
-                <Link to="/contact" className="flat-button">Contact Me</Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>Hi, I'm Jacob Hupfer</h1>
+        {/* <AnimatedLetters letterClass={letterClass} */}
+        {/* strArray={nameArray} */}
+        {/* idx={15}/> */}
+        <h2>
+          Frontend Developer with heavy interest in learning more backend
+          concepts
+        </h2>
+        <Link to="/contact" className="flat-button">
+          Contact Me
+        </Link>
+      </div>
+      <Logo />
+    </div>
+  )
 }
 
 export default Home
