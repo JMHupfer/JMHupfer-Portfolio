bcf5434c9cd909d0bb46044ca3a25dec3c697c8b
JMHupfer <jmhupfer@gmail.com>
2023-10-22 03:20:10 -0600

    working on about me page

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import './index.scss'
+
+const About = () => () => {
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>The Code Explorer: Jacob Hupfer</h1>
+        <p>
+          As an avid traveller I have always been interested in different
+          cultures, languages, and perspectives and am a firm believer that the
+          world needs to get closer together. I never knew what path I wanted to
+          take to further enhance globalization but while I was living in China
+          before the pandemic I realized that tech was the way to go. I love the
+          idea of technology bridging the gap between nations and I cannot wait
+          to begin my tech journey and see what paths open up along the way!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
